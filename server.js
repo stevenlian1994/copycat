1ec4f39216956db120cd9ea98c8a8b3a769801fb
Steven Lian <30307988+stevenlian1994@users.noreply.github.com>
2019-07-01 20:03:59 -0700

    added angular project

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express()
+
+app.use(express.static( __dirname + '/public/dist/public' ));
 // var mongoose = require('mongoose')
 var server = app.listen(8000, function(){
     console.log("listening on port 8000")
