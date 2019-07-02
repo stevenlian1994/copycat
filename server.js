fc6322a293c2bfceb3b0ed079bf41cf64b49170d
Steven Lian <30307988+stevenlian1994@users.noreply.github.com>
2019-07-01 19:33:21 -0700

    added gitignore and server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express')
+var app = express()
+// var mongoose = require('mongoose')
+var server = app.listen(8000, function(){
+    console.log("listening on port 8000")
+})
